aa6741eb2b4283e6313bc3d1f20ba6daa7e0674a
401adii <126412203+401adii@users.noreply.github.com>
2024-12-21 18:00:02 +0100

    Added amd configured native wind

---
+++ app/app/App.jsx
@@ -0,0 +1,13 @@
+import Component from "./Component";
+import "../global.css"
+import { SafeAreaView } from "react-native-web";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+        <Component/>
+    </SafeAreaView>
+  );
+}
+
+
