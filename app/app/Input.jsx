4a75c7f6cecf62359061b12e55a3dc65d126b78f
401adii <126412203+401adii@users.noreply.github.com>
2024-12-22 13:31:30 +0100

    Prebuilded app and added bluetooth classic library

--- app/app/Input.jsx
+++ app/app/Input.jsx
@@ -3,14 +3,20 @@ import React from 'react'
 import { useState } from 'react';
 
 const Input = () => {
-    const [text, onChangeText] = useState("");
+
+    const [text, setText] = useState("");
+    
+
+
+
     return (
         <View className="flex-1 bg-gray-900 items-center justify-center"> 
-            <Text className="m-1 text-stone-200 font-bold">Enter your personal data</Text>
-            <TextInput className="m-4 bg-gray-200 w-1/2 h-1/8 p-4 rounded" onChangeText={onChangeText}></TextInput>
-            <Button className="m-4 p-4 rounded" onPress={() => Alert.alert("sending data")} title="SEND DATA"/>
+            <Text className="m-1 text-stone-200 font-bold">Enter your name</Text>
+            <TextInput className="m-4 bg-gray-200 w-1/2 h-1/8 p-4 rounded" onChangeText={setText} value={text}/>
+            <Button className="m-4 p-4 rounded" onPress={() => setText("")} title="SEND DATA"/>
         </View>
     )
+
 }
 
 export default Input
