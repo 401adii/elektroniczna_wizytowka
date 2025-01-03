38bda2a3238c1a7d2bf71a932b07d1ec0d0b5ab7
netkowski <netkowskinikodem@gmail.com>
2025-01-04 00:19:49 +0100

    partial refresh

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,20 @@ const char text1[] = "napis1";
 const char text2[] = "napis2";
 const char* aktualnyNapis = text1;
 
-void displayText(const char* text)
-{
+void displayFrame() {
+
+  display.setFullWindow();
+  display.firstPage();
+  do {
+    display.fillScreen(GxEPD_WHITE);
+    for (int i = 0; i < 50; i++) {
+      display.drawRect(i, i, display.width() - 2 * i, display.height() - 2 * i, GxEPD_BLACK);
+    }
+  } while (display.nextPage());
+}
+
+void displayTextPartial(const char* text) {
+
   int16_t tbx, tby;
   uint16_t tbw, tbh;
   display.getTextBounds(text, 0, 0, &tbx, &tby, &tbw, &tbh);
@@ -22,47 +34,38 @@ void displayText(const char* text)
   uint16_t frameWidth = tbw + 20; 
   uint16_t frameHeight = tbh + 20;
 
-  display.setFullWindow();
+  display.setPartialWindow(frameX, frameY, frameWidth, frameHeight);
   display.firstPage();
-  do
-  {
+  do {
     display.fillScreen(GxEPD_WHITE);
-
-    for (int i = 0; i < 5; i++) {
-      display.drawRect(i, i, display.width() - 2 * i, display.height() - 2 * i, GxEPD_BLACK);
-    }
-
     display.drawRect(frameX, frameY, frameWidth, frameHeight, GxEPD_BLACK);
-    
-  
+
     display.setCursor(x, y);
     display.print(text);
-  } 
-  while (display.nextPage());
+  } while (display.nextPage());
 }
 
-void setup()
-{
+void setup() {
   pinMode(PIN_ENABLE, OUTPUT);
   digitalWrite(PIN_ENABLE, HIGH);
   display.init(115200, true, 2, false);
   display.setRotation(1);
   display.setFont(&FreeMonoBold9pt7b);
   display.setTextColor(GxEPD_BLACK);
+
+
+  displayFrame();
 }
 
-void loop()
-{
-  displayText(aktualnyNapis);
+void loop() {
 
-  if (aktualnyNapis == text1)
-  {
+  displayTextPartial(aktualnyNapis);
+
+  if (aktualnyNapis == text1) {
     aktualnyNapis = text2;
-  }
-  else
-  {
+  } else {
     aktualnyNapis = text1;
   }
 
-  delay(2000); 
+  delay(1);
 }
