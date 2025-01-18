069c1d0d77612aa2eadcfb8656e7de6cb64bbd06
401adii <126412203+401adii@users.noreply.github.com>
2025-01-19 00:15:58 +0100

    Added QR svg package

--- app/app/App.jsx
+++ app/app/App.jsx
@@ -1,12 +1,9 @@
 import '../global.css'
-import {Text} from 'react-native'
-import {useState, useEffect} from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import TestView from './TestView';
 import ConnectionScreen from './screens/ConnectionScreen';
 import ChoiceScreen from './screens/ChoiceScreen';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   
@@ -14,7 +11,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator animationTypeForReplace='push' initialRouteName='ConnectionScreen'>
+      <Stack.Navigator animationTypeForReplace='push' initialRouteName='test'>
         <Stack.Screen name='test' component={TestView}/>
         <Stack.Screen name='ConnectionScreen' options={{title: 'Connect to a device'}} component={ConnectionScreen}/>
         <Stack.Screen name='ChoiceScreen' options={{title: 'Choose screens'}} component={ChoiceScreen}/>
